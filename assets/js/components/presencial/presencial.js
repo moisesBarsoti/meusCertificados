bf9b4aff9b2fccd1a6261acad3bf5757f3ea6994
moisesBarsoti <moises.barsot2@gmail.com>
2025-04-20 12:19:07 -0300

    Atualizado a seção de estatisticas do index.html foi trocado as cores

--- assets/js/components/presencial/presencial.js
+++ assets/js/components/presencial/presencial.js
@@ -21,30 +21,3 @@ document.querySelectorAll('header nav ul li a').forEach(link => {
         }
     });
 });
-
-// Função para criar o efeito de digitação
-function typeWriterEffect(element, text, speed) {
-    let index = 0;
-
-    function type() {
-        if (index < text.length) {
-            element.textContent += text.charAt(index); // Adiciona uma letra por vez
-            index++;
-            setTimeout(type, speed); // Chama a função novamente após o intervalo
-        }
-    }
-
-    type();
-}
-
-// Seleciona o elemento <h1>
-const targetElement = document.querySelector('h1');
-
-// Texto que será exibido letra por letra
-const text = "Certificados Presenciais";
-
-// Velocidade do efeito (em milissegundos)
-const speed = 100; // 100ms entre cada letra
-
-// Inicia o efeito
-typeWriterEffect(targetElement, text, speed);
